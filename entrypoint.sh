0895097d79c6a7a264a9ce83fd55dd07fd596195
pooja.bera <pooja.bera@clearstream.com>
2021-11-19 14:34:13 +0000

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 # get regsistration token for this runnner
-ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/PoojaBeraClearstream/youtube-gcloud-continuous-deployment/actions/runners/registration-token" --header "authorization: Bearer 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"  --header 'content-type: application/json' | jq -r .token)"
+ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/PoojaBeraClearstream/youtube-gcloud-continuous-deployment/actions/runners/registration-token" --header "authorization: Bearer AVCJM2GVQL2T6VYFAXEQ64DBS62VW"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
 /runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
