9d6cc58148bda02473ecc40c5b0dc8d7380e226e
pooja.bera <pooja.bera@clearstream.com>
2021-11-19 09:07:49 +0000

    commit!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 ACTIONS_RUNNER_INPUT_NAME=$HOSTNAME
 # get regsistration token for this runnner
-ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${GITHUB_TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
+ACTIONS_RUNNER_INPUT_TOKEN="$(curl -sS --request POST --url "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runners/registration-token" --header "authorization: Bearer ${TOKEN}"  --header 'content-type: application/json' | jq -r .token)"
 # configure runner
 export RUNNER_ALLOW_RUNASROOT=1
 /runner/config.sh --unattended --replace --work "/tmp" --url "$ACTIONS_RUNNER_INPUT_URL" --token "$ACTIONS_RUNNER_INPUT_TOKEN" --labels k8s-runner
