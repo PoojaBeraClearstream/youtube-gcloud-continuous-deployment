df28bcceb28d693f0c6cf9b68de47915bb2074ce
pooja.bera <pooja.bera@clearstream.com>
2021-11-30 09:31:08 +0000

    hb

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 registration_url="https://api.github.com/repos/PoojaBeraClearstream/youtube-gcloud-continuous-deployment/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
 
-payload=$(curl -sX POST -H "Authorization: token ${GIT_TOKEN}" ${registration_url})
-export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+payload=$(curl -sX POST -H "Authorization: Bearer ${GIT_TOKEN}" ${registration_url})
+export RUNNER_TOKEN=$(echo $payload | jq -r .token --raw-output)
 export RUNNER_ALLOW_RUNASROOT=1
 ./config.sh \
     --name $(hostname) \
