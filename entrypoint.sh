aad16148a3910269274f370d390fcaf2f1938181
pooja.bera <pooja.bera@clearstream.com>
2021-12-02 08:53:02 +0000

    test19

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export RUNNER_TOKEN=$(echo $payload | jq -r .token --raw-output)
 export RUNNER_ALLOW_RUNASROOT=1
 ./config.sh \
     --name $(hostname) \
-    --token $RUNNER_TOKEN   \
+    --token "AVCJM2CGIF5WS3S4WBVUOLLBVCLV4"   \
     --url https://github.com/PoojaBeraClearstream/youtube-gcloud-continuous-deployment \
     --work runner \
     --unattended \
